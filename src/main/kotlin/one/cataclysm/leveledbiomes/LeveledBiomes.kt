aee5f013318aa6281c24f00ab6514f87ca0f0b19
auramass <maxz75.04@gmail.com>
2022-11-07 09:53:09 +0200

    Marked Issue with Config loader

--- src/main/kotlin/one/cataclysm/leveledbiomes/LeveledBiomes.kt
+++ src/main/kotlin/one/cataclysm/leveledbiomes/LeveledBiomes.kt
@@ -1,6 +1,5 @@
 package one.cataclysm.leveledbiomes
 
-import one.cataclysm.leveledbiomes.config.MyConfigLoader
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -11,5 +10,4 @@ val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
 
 @Suppress("unused")
 fun onInitialize() {
-    MyConfigLoader
 }
--- src/main/kotlin/one/cataclysm/leveledbiomes/config/MyConfigLoader.kt
+++ src/main/kotlin/one/cataclysm/leveledbiomes/config/MyConfigLoader.kt
@@ -4,6 +4,7 @@ import com.sksamuel.hoplite.ConfigLoaderBuilder
 import com.sksamuel.hoplite.addResourceSource
 
 object MyConfigLoader {
+    //TODO handle on throw exception
     val config: Config = ConfigLoaderBuilder.default().addResourceSource("leveledbiomes.yml").build().loadConfigOrThrow()
 
     //TODO
